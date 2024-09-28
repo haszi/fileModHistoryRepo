ee099da1e72e4b55c681cda905ee5451194be1e2
haszi <haszika80@gmail.com>
2024-09-28 18:31:24 +0200

    Initial commit

---
+++ fileModHistory-repo1.php
@@ -0,0 +1,31 @@
+<?php
+
+/* This is a generated file */
+
+return [
+    "last commit hash" => "664586a43d9738d7b69a2bb2d621e0c1a8fc9068",
+    ".github/workflows/historyInDocsRepo.yml" => [
+        "modified" => "2024-07-21T18:32:23+02:00",
+        "contributors" => [
+            "haszi",
+        ],
+    ],
+    "firstTestFile.md" => [
+        "modified" => "2024-07-21T18:33:18+02:00",
+        "contributors" => [
+            "haszi",
+        ],
+    ],
+    "scripts/updateModHistory.php" => [
+        "modified" => "2024-07-21T18:32:23+02:00",
+        "contributors" => [
+            "haszi",
+        ],
+    ],
+    "fileModHistory.php" => [
+        "modified" => "2024-07-21T16:25:46Z",
+        "contributors" => [
+            "github-actions[bot]",
+        ],
+    ],
+];
